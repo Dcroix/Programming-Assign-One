6aec81a04f72f1b5b6906d63f213f9d11d9295fd
Dcroix <pghilado@up.edu.ph>
2019-04-21 17:20:56 +0800

    Revision in Files

--- observation.R
+++ observation.R
@@ -1,3 +1,7 @@
+#Reads a directory full of files and reports the number of completely observed cases in each data file. 
+#The function should return a data frame where the first column is the name of the file and the second 
+#column is the number of complete cases.
+
 observation <- function (directory, id = 1:332){
   files <- list.files(directory, full.names =1)
   data <- data.frame(id = integer(), nobs = integer())
